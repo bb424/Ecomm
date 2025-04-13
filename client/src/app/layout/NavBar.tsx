bc0f9f35237bb590f151b984cc487c17e61af9f6
bb424 <brad9m@yahoo.com>
2025-04-12 22:03:16 -0400

    End of section 3

---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,22 @@
+import { DarkMode, LightMode } from "@mui/icons-material";
+import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+
+type Props = {
+    darkMode: boolean;
+    handleThemeToggle: () => void;
+}
+
+export default function NavBar({ darkMode, handleThemeToggle }: Props) {
+
+
+    return (
+        <AppBar position='fixed' >
+            <Toolbar>
+                <Typography variant="h6">SNOWBOARD SHOP</Typography>
+                <IconButton onClick={handleThemeToggle} aria-label="toggle theme">
+                    {darkMode ? <DarkMode /> : <LightMode sx={{ color: 'yellow' }} />}
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    )
+}
